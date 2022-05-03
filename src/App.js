2ac647f0e373c05eaa0739929561dc08b4c73a1d
HoSySon1802 <hosysoncntt6@gmail.com>
2022-05-03 18:18:30 +0700

    Blockchahin final

--- src/App.js
+++ src/App.js
@@ -1,23 +1,117 @@
-import logo from './logo.svg';
 import './App.css';
+import Web3 from "web3";
+import { useCallback, useState, useEffect } from 'react';
+import detectEthereumProvider from "@metamask/detect-provider";
+import { loadContract } from "./utils/loadcontract";
+import React, { Component }  from 'react';
+
+
 
 function App() {
+
+  const [web3Api, setWeb3Api] = useState({
+    provider: null,
+    web3: null,
+    contract: null,
+  });
+
+  const [account, setAccount] = useState(null);
+  const [balance, setBalance] = useState(null);
+
+  const [shouldReload, reload] = useState(false);
+  const reloadEffect = () => reload(!shouldReload)
+
+  const setAccountLister = (provider) => {
+    provider.on("accountChanged", accounts => setAccount(accounts[0]))
+  }
+
+  useEffect(() => {
+    const loadProvider = async () => {
+      const provider = await detectEthereumProvider()
+      const contract = await loadContract("Faucet", provider)
+
+      debugger
+
+      if (provider) {
+        setAccountLister(provider)
+        setWeb3Api({
+          web3: new Web3(provider),
+          provider,
+          contract
+        })
+      } else {
+        console.error("please, Install Metamask")
+      }
+    }
+    loadProvider()
+  }, []);
+
+  useEffect(() => {
+    const getAccount = async () => {
+      const accounts = await web3Api.web3.eth.getAccounts()
+      setAccount(accounts[0])
+    }
+    web3Api.web3 && getAccount() && reloadEffect()
+  }, [web3Api.web3]);
+
+  useEffect(() => {
+    const loadBalance = async () => {
+      const { contract, web3 } = web3Api
+      const balance = await web3.eth.getBalance(contract.address)
+      setBalance(web3.utils.fromWei(balance, "ether"))
+
+    }
+    web3Api.contract && loadBalance()
+  }, [web3Api, shouldReload]);
+
+  const addFunds = useCallback(async () => {
+    const { contract, web3 } = web3Api
+    await contract.addFunds({
+      from: account,
+      value: web3.utils.toWei("1", "ether")
+    })
+    reloadEffect()
+  }, [web3Api, account])
+
+
+  const withdraw = async () => {
+    const { contract, web3 } = web3Api
+    const withdrawAmount = web3.utils.toWei("0.5", "ether")
+    await contract.withdraw(withdrawAmount, {
+      from: account
+    })
+    reloadEffect()
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>{balance}</strong> ETH
+        </div>
+        <button className="button is-primary mr-5"
+          onClick={addFunds}
+        >Donate</button>
+        <button className="button is-danger mr-5"
+          onClick={withdraw}
+        >Withdraw</button>
+        <button className="button is-link"
+          onClick={() =>
+            web3Api.provider.request({ method: "eth_requestAccounts" })
+          }
         >
-          Learn React
-        </a>
-      </header>
+          Connect Wallets
+        </button>
+        <span>
+          <p>
+            <strong>Accounts Address: </strong>
+            {
+              account ? account : "Accounts Denined"
+            }
+          </p>
+        </span>
+      </div>
     </div>
   );
 }
